537a34bee42d319b69ef4e23bffb48c5e149e7c3
zachvalenta <zjv5002@gmail.com>
2020-05-29 19:40:15 -0400

    rm previous quote module

--- hello.go
+++ hello.go
@@ -1,12 +1,11 @@
 package hello
 
 import (
-    "rsc.io/quote"
     quoteV3 "rsc.io/quote/v3"
 )
 
 func Hello() string {
-    return quote.Hello()
+    return quoteV3.HelloV3()
 }
 
 func Proverb() string {
